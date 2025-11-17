098ea1f31585020e6b6ebcc25507da0409f80e4f
Amal Sibu Thottam <amalsibu1@gmail.com>
2025-11-17 14:10:11 -0500

    Portfolio created
    Added description

---
+++ Portfolio.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+    strings:["Computer Science Student at York University", "Aspiring Software Engineer", "Web Developer", "Frontend Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+})
